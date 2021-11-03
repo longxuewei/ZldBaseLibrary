68f013de34e696fd70773e285d17a1677768fcab
Lxw <China2021@126.com>
2021-11-03 18:04:34 +0800

    日志工具优化！

---
+++ zldbaselibrary/src/main/java/com/library/zldbaselibrary/util/L.java
@@ -0,0 +1,22 @@
+package com.library.zldbaselibrary.util;
+
+import android.util.Log;
+
+public class L {
+
+    public static String TAG = "TAG";
+
+    public static void d(String log) {
+        Log.d(TAG, log);
+    }
+
+    public static void e(String log) {
+        Log.d(TAG, log);
+    }
+
+    public static void i(String log) {
+        Log.d(TAG, log);
+    }
+
+
+}
